57394c68578b3ff9694f26b9dba413d3ac1812c5
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2026-01-31 20:17:27 +0300

    Добавьте файлы проекта.

---
+++ WpfApp_Book/1_window.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp_Book
+{
+    /// <summary>
+    /// Логика взаимодействия для _1_window.xaml
+    /// </summary>
+    public partial class _1_window : Window
+    {
+        public _1_window()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfApp_Book/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_Book
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_1_Click(object sender, RoutedEventArgs e)
+        {
+            _1_window _1_Window = new _1_window();
+            _1_Window.Show();
+        }
+
+        private void Button_2_Click(object sender, RoutedEventArgs e)
+        {
+            _2_window _2_Window = new _2_window();
+            _2_Window.Show();
+        }
+    }
+}
